1a382cfcbb53c24821defcf2b4fdb873ef14083a
egg <zacharie-cote@hotmail.ca>
2025-06-22 12:54:24 -0400

    Add serial port support, implement test framework, and expand VGA buffer tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,31 +1,26 @@
+//basic_boot.rs
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(zach_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(zach_os::test_runner)]
 
 use core::panic::PanicInfo;
 use zach_os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     zach_os::test_panic_handler(info)
